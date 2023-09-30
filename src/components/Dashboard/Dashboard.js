e3bc3e131dbbfe5eed5c5946a08e43a898d7facb
Srinivas <srinivas365@proton.me>
2023-09-30 09:58:24 +0530

    Intial commit

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+export function Dashboard() {
+    return (
+        <div>
+            <span>Total Calories Burned: 5</span>
+            <span>Total Calories Consumed: 15</span>
+            <span>Total Calories Goal: 10</span>
+            <span>Remaining Calories to Goal: 5</span>
+        </div>
+    );
+}
